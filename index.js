b066c80b6a03b91b291e0539b7abe0fa0a2b009e
Gavin Quirk <gavinquirk@gmail.com>
2021-10-27 13:21:29 -0700

    separated route file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Router
+app.use('/api', require('./routes'));
 
 // Enable CORS
 app.use(cors);
